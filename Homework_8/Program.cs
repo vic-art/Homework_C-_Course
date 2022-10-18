6b0baae5b5bdff3e129f8e00fddae73c823e0a58
Victoria Artyushkina <victoriaartyushkina@gmail.com>
2022-10-19 01:58:45 +0200

    Add solution to Homework_8

--- Homework_8/Program.cs
+++ Homework_8/Program.cs
@@ -9,88 +9,61 @@
 // 9 5 3 2
 // 8 4 4 2
 
-// int m = 3, n = 4;
+int m = 3, n = 4;
 
-// int[,] array = CreateArray(m, n);
-// int[,] sortedArray = Sorting(array);
-// Console.WriteLine();
-// PrintArray(sortedArray);
-
-
-// int[,] CreateArray(int _m, int _n, int max = 10, int min = 0)
-// {
-//     int[,] _array = new int[_m, _n];
-//     for (int i = 0; i < _array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < _array.GetLength(1); j++)
-//         {
-//             _array[i, j] = new Random().Next(min, max);
-//             Console.Write($"{_array[i, j]} ");
-//         }
-//         Console.WriteLine();
-//     }
-//     return _array;
-// }
-
-// int[,] Sorting(int[,] _array)
-// {
-//     for (int i = 0; i < _array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < _array.GetLength(1) - 1; j++)
-//         {
-//             for (int k = 0; k < _array.GetLength(1) - j - 1; k++)
-//             {
-//                 if (_array[i, k] < _array[i, k + 1])
-//                 {
-//                     int temp = _array[i, k];
-//                     _array[i, k] = _array[i, k + 1];
-//                     _array[i, k + 1] = temp;
-//                 }
-//             }
-
-//         }
-//     }
-//     return _array;
-// }
-
-// void PrintArray(int[,] _array)
-// {
-//     for (int i = 0; i < _array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < _array.GetLength(1); j++)
-//         {
-//             Console.Write($"{_array[i, j]} ");
-//         }
-//         Console.WriteLine();
-//     }
-// }
-
-// В сортировке методом пузырька количество итераций внешнего цикла определяется длинной списка 
-// минус единица, так как когда второй элемент становится на свое место, то первый уже однозначно 
-// минимальный и находится на своем месте.
-
-// Количество итераций внутреннего цикла зависит от номера итерации внешнего цикла, так как конец 
-// списка уже отсортирован, и выполнять проход по этим элементам смысла нет.
+int[,] array = CreateArray(m, n);
+int[,] sortedArray = Sorting(array);
+Console.WriteLine();
+PrintArray(sortedArray);
 
 
+int[,] CreateArray(int _m, int _n, int max = 10, int min = 0)
+{
+    int[,] _array = new int[_m, _n];
+    for (int i = 0; i < _array.GetLength(0); i++)
+    {
+        for (int j = 0; j < _array.GetLength(1); j++)
+        {
+            _array[i, j] = new Random().Next(min, max);
+            Console.Write($"{_array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    return _array;
+}
 
+int[,] Sorting(int[,] _array)
+{
+    for (int i = 0; i < _array.GetLength(0); i++)
+    {
+        for (int j = 0; j < _array.GetLength(1) - 1; j++)
+        {
+            for (int k = 0; k < _array.GetLength(1) - j - 1; k++)
+            {
+                if (_array[i, k] < _array[i, k + 1])
+                {
+                    int temp = _array[i, k];
+                    _array[i, k] = _array[i, k + 1];
+                    _array[i, k + 1] = temp;
+                }
+            }
 
+        }
+    }
+    return _array;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void PrintArray(int[,] _array)
+{
+    for (int i = 0; i < _array.GetLength(0); i++)
+    {
+        for (int j = 0; j < _array.GetLength(1); j++)
+        {
+            Console.Write($"{_array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
 
 
 
@@ -109,49 +82,49 @@
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой 
 // элементов: 1 строка
 
-// int m = 4, n = 4;
+int m = 4, n = 4;
 
-// int[,] array = CreateArray(m, n);
+int[,] array = CreateArray(m, n);
 
-// SmallestSum(array);
+SmallestSum(array);
 
-// int[,] CreateArray(int _m, int _n, int min = 0, int max = 10)
-// {
-//     int[,] _array = new int[_m, _n];
-//     for (int i = 0; i < _m; i++)
-//     {
-//         for (int j = 0; j < _n; j++)
-//         {
-//             _array[i, j] = new Random().Next(min, max);
-//             Console.Write($"{_array[i, j]} ");
-//         }
-//         Console.WriteLine();
-//     }
-//     return _array;
+int[,] CreateArray(int _m, int _n, int min = 0, int max = 10)
+{
+    int[,] _array = new int[_m, _n];
+    for (int i = 0; i < _m; i++)
+    {
+        for (int j = 0; j < _n; j++)
+        {
+            _array[i, j] = new Random().Next(min, max);
+            Console.Write($"{_array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    return _array;
 
-// }
+}
 
-// void SmallestSum(int[,] _array)
-// {
-//     int min = 0;
-//     int rowNumber = 1;
-//     for (int i = 0; i < _array.GetLength(0); i++)
-//     {
-//         int sum = 0;
-//         for (int k = 0; k < _array.GetLength(1); k++)
-//         {
-//             sum += _array[i, k];
-//         }
-//         if (i == 0)
-//             min = sum;
-//         else if (sum < min)
-//         {
-//             min = sum;
-//             rowNumber = i + 1;
-//         }
-//     }
-//     Console.WriteLine($"Номер строки с наименьшей суммой элементов: {rowNumber} строка");
-// }
+void SmallestSum(int[,] _array)
+{
+    int min = 0;
+    int rowNumber = 1;
+    for (int i = 0; i < _array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int k = 0; k < _array.GetLength(1); k++)
+        {
+            sum += _array[i, k];
+        }
+        if (i == 0)
+            min = sum;
+        else if (sum < min)
+        {
+            min = sum;
+            rowNumber = i + 1;
+        }
+    }
+    Console.WriteLine($"Номер строки с наименьшей суммой элементов: {rowNumber} строка");
+}
 
 
 
@@ -164,61 +137,58 @@
 // 18 20
 // 15 18
 
-// int m, n, k;
+int m, n, k;
 
-// Console.WriteLine("Введите число строк 1-ой матрицы");
-// int.TryParse(Console.ReadLine()!, out m);
+Console.WriteLine("Введите число строк 1-ой матрицы");
+int.TryParse(Console.ReadLine()!, out m);
 
-// Console.WriteLine("Введите число столбцов 1-ой матрицы (и строк 2-ой матрицы)");
-// int.TryParse(Console.ReadLine()!, out n);
+Console.WriteLine("Введите число столбцов 1-ой матрицы (и строк 2-ой матрицы)");
+int.TryParse(Console.ReadLine()!, out n);
 
-// Console.WriteLine("Введите число столбцов 2-ой матрицы");
-// int.TryParse(Console.ReadLine()!, out k);
+Console.WriteLine("Введите число столбцов 2-ой матрицы");
+int.TryParse(Console.ReadLine()!, out k);
 
-// Console.WriteLine("Первая матрица");
-// int[,] matrix1 = CreateArray(m, n);
-// Console.WriteLine("Вторая матрица");
-// int[,] matrix2 = CreateArray(n, k);
-// Console.WriteLine("Результат умножения матриц");
-// dotProduct(matrix1, matrix2);
-
-
-// int[,] CreateArray(int _m, int _n, int min = 0, int max = 5)
-// {
-//     int[,] _array = new int[_m, _n];
-//     for (int i = 0; i < _array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < _array.GetLength(1); j++)
-//         {
-//             _array[i, j] = new Random().Next(min, max);
-//             Console.Write($"{_array[i, j]} ");
-//         }
-//         Console.WriteLine();
-//     }
-//     return _array;
-// }
-
-// void dotProduct(int[,] _matrix1, int[,] _matrix2)
-// {
-//     int[,] _result = new int[m, k];
-//     for (int i = 0; i < _result.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < _result.GetLength(1); j++)
-//         {
-//             int sum = 0;
-//             for (int l = 0; l < _matrix1.GetLength(1); l++)
-//             {
-//                 sum += _matrix1[i, l] * _matrix2[l, j];
-//             }
-//             _result[i, j] = sum;
-//             Console.Write($"{_result[i, j]} ");
-//         }
-//         Console.WriteLine();
-//     }
-// }
+Console.WriteLine("Первая матрица");
+int[,] matrix1 = CreateArray(m, n);
+Console.WriteLine("Вторая матрица");
+int[,] matrix2 = CreateArray(n, k);
+Console.WriteLine("Результат умножения матриц");
+dotProduct(matrix1, matrix2);
 
 
+int[,] CreateArray(int _m, int _n, int min = 0, int max = 5)
+{
+    int[,] _array = new int[_m, _n];
+    for (int i = 0; i < _array.GetLength(0); i++)
+    {
+        for (int j = 0; j < _array.GetLength(1); j++)
+        {
+            _array[i, j] = new Random().Next(min, max);
+            Console.Write($"{_array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    return _array;
+}
 
+void dotProduct(int[,] _matrix1, int[,] _matrix2)
+{
+    int[,] _result = new int[m, k];
+    for (int i = 0; i < _result.GetLength(0); i++)
+    {
+        for (int j = 0; j < _result.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int l = 0; l < _matrix1.GetLength(1); l++)
+            {
+                sum += _matrix1[i, l] * _matrix2[l, j];
+            }
+            _result[i, j] = sum;
+            Console.Write($"{_result[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
 
 
 
@@ -234,27 +204,53 @@
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
-// int m = 2, n = 2, k = 2;
 
-// int[,,] array = CreateArray(m, n, k);
+int[,,] array = new int[2, 2, 2];
 
-// int[,,] CreateArray(int _m, int _n, int _k, int min = 10, int max = 100)
-// {
-//     int[,,] _array = new int[_m, _n, _k];
-//     for (int i = 0; i < _array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < _array.GetLength(1); j++)
-//         {
-//             for (int l = 0; l < _array.GetLength(2); l++)
-//             {
-//                 _array[i, j, l] = new Random().Next(min, max);
-//                 Console.Write($" {_array[i, j, l]}({l}, {j}, {i}) ");
-//             }
-//             Console.WriteLine();
-//         }
-//     }
-//     return _array;
-// }
+List<int> nums = new List<int>();
+
+for (int i = 0; i < 20; i++)
+    nums.Add(i + 10);
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int k = 0; k < array.GetLength(2); k++)
+        {
+            if (nums.Count == 0)
+                array[i, j, k] = 0;
+            else
+            {
+                int temp = new Random().Next(nums.Count);
+                array[i, j, k] = nums[temp];
+                nums.RemoveAt(temp);
+            }
+        }
+    }
+}
+
+
+WriteArray(array);
+
+
+void WriteArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"x({i}) y({j}) ");
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"z({k})={array[i, j, k]}; ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
 
 
 
@@ -265,23 +261,38 @@
 // 11 16 15 06
 // 10 09 08 07
 
-int m = 4, n = 4;
 
-int[,] CreateArray(int _m, int _n, int max, int min)
+int m = 4;
+
+int[,] array = CreateArray(m);
+PrintArray(array);
+
+int[,] CreateArray(int _m)
 {
-    int[,] _array = new int[_m, _n];
-    for (int i = 0; i < _array.GetLength(0); i++)
+    int[,] _array = new int[_m, _m];
+    for (int i = 0, j = 0, k = 0; k < _array.Length; k++)
     {
-
+        _array[i, j] = k + 1;
+        if (i <= j + 1 && i + j < _array.GetLength(1) - 1)
+            j++;  // вправо
+        else if (i < j && i + j >= _array.GetLength(0) - 1)
+            i++;  // вниз
+        else if (i >= j && i + j > _array.GetLength(1) - 1)
+            j--; // влево
+        else
+            i--; // вверх
     }
+    return _array;
 }
 
-
-
-
-
-
-
-
-
-
+void PrintArray(int[,] _array)
+{
+    for (int i = 0; i < _array.GetLength(0); i++)
+    {
+        for (int j = 0; j < _array.GetLength(1); j++)
+        {
+            Console.Write(string.Format("{0:00} ", _array[i, j]));
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Homework_9/Program.cs
@@ -0,0 +1,82 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все 
+// натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+// int N;
+// Console.WriteLine("Введите число: ");
+// int.TryParse(Console.ReadLine()!, out N);
+
+// Console.WriteLine(PrintNumbers(N));
+
+// string PrintNumbers(int _N)
+// {
+//     if (_N == 1)
+//         return 1.ToString();
+//     return $"{_N} {PrintNumbers(_N - 1)}";
+// }
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// double M, N;
+// Console.WriteLine("Введите первое число ");
+// double.TryParse(Console.ReadLine()!, out M);
+
+// Console.WriteLine("Введите второе число ");
+// double.TryParse(Console.ReadLine()!, out N);
+
+// Console.WriteLine(Sum((int)M, (int)N));
+
+// int Sum(int a, int b)
+// {
+//     if (a == b)
+//         return b;
+//     return a + Sum(a + 1, b);
+// }
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с 
+// помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int M, N;
+Console.WriteLine("Введите первое неотрицательное число ");
+int.TryParse(Console.ReadLine()!, out M);
+
+Console.WriteLine("Введите второе неотрицательное число ");
+int.TryParse(Console.ReadLine()!, out N);
+
+int AckermannFunction(int _M, int _N)
+{
+    if (_M = 0)
+    {
+        return _N + 1;
+    }
+    else if (_M > 0 && _N == 0)
+    {
+        return
+    }
+    else if (_M > 0 && _N > 0)
+    {
+
+    }
+    else
+        Console.WriteLine("Функция Аккермана определена для неотрицательных целых чисел. Измените вводные данные.");
+
+}
+
+
+
+
+
+
+
+
+
+

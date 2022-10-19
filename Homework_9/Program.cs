85d967ad40b0af942170b45b283a9474d6638f77
Victoria Artyushkina <victoriaartyushkina@gmail.com>
2022-10-19 03:33:56 +0200

    Add the solution to Homework_9

--- Homework_9/Program.cs
+++ Homework_9/Program.cs
@@ -4,18 +4,18 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-// int N;
-// Console.WriteLine("Введите число: ");
-// int.TryParse(Console.ReadLine()!, out N);
+int N;
+Console.WriteLine("Введите число: ");
+int.TryParse(Console.ReadLine()!, out N);
 
-// Console.WriteLine(PrintNumbers(N));
+Console.WriteLine(PrintNumbers(N));
 
-// string PrintNumbers(int _N)
-// {
-//     if (_N == 1)
-//         return 1.ToString();
-//     return $"{_N} {PrintNumbers(_N - 1)}";
-// }
+string PrintNumbers(int _N)
+{
+    if (_N == 1)
+        return 1.ToString();
+    return $"{_N} {PrintNumbers(_N - 1)}";
+}
 
 // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
 // сумму натуральных элементов в промежутке от M до N.
@@ -23,21 +23,21 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-// double M, N;
-// Console.WriteLine("Введите первое число ");
-// double.TryParse(Console.ReadLine()!, out M);
+double M, N;
+Console.WriteLine("Введите первое число ");
+double.TryParse(Console.ReadLine()!, out M);
 
-// Console.WriteLine("Введите второе число ");
-// double.TryParse(Console.ReadLine()!, out N);
+Console.WriteLine("Введите второе число ");
+double.TryParse(Console.ReadLine()!, out N);
 
-// Console.WriteLine(Sum((int)M, (int)N));
+Console.WriteLine(Sum((int)M, (int)N));
 
-// int Sum(int a, int b)
-// {
-//     if (a == b)
-//         return b;
-//     return a + Sum(a + 1, b);
-// }
+int Sum(int a, int b)
+{
+    if (a == b)
+        return b;
+    return a + Sum(a + 1, b);
+}
 
 
 // Задача 68: Напишите программу вычисления функции Аккермана с 
@@ -46,28 +46,42 @@
 // m = 3, n = 2 -> A(m,n) = 29
 
 int M, N;
+
 Console.WriteLine("Введите первое неотрицательное число ");
 int.TryParse(Console.ReadLine()!, out M);
+while (M < 0)
+{
+    Console.WriteLine("Неправильный ввод. Введите неотрицательное число. ");
+    int.TryParse(Console.ReadLine()!, out M);
+}
 
 Console.WriteLine("Введите второе неотрицательное число ");
 int.TryParse(Console.ReadLine()!, out N);
-
-int AckermannFunction(int _M, int _N)
+while (N < 0)
 {
-    if (_M = 0)
-    {
-        return _N + 1;
-    }
-    else if (_M > 0 && _N == 0)
-    {
-        return
-    }
-    else if (_M > 0 && _N > 0)
-    {
+    Console.WriteLine("Неправильный ввод. Введите неотрицательное число. ");
+    int.TryParse(Console.ReadLine()!, out N);
+}
 
+Console.WriteLine($"Результат: {ack(M, N)}");
+
+int ack(int m, int n)
+{
+
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if ((m > 0) && (n == 0))
+    {
+        return ack(m - 1, 1);
+    }
+    else if ((m > 0) && (n > 0))
+    {
+        return ack(m - 1, ack(m, n - 1));
     }
     else
-        Console.WriteLine("Функция Аккермана определена для неотрицательных целых чисел. Измените вводные данные.");
+        return n + 1;
 
 }
 
